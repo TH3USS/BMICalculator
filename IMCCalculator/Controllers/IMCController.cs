e352df1b2b933ce54fad64649c3de7a8bb409d62
Matheus <matheusramossilva2003@gmail.com>
2024-12-26 19:46:18 -0300

    upload

---
+++ IMCCalculator/Controllers/IMCController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IMCCalculator.Models;
+
+namespace IMCCalculator.Controllers
+{
+    public class IMCController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new IMCModel());
+        }
+
+        [HttpPost]
+        public IActionResult Calcular(IMCModel model)
+        {
+            if (model.Peso > 0 && model.Altura > 0)
+            {
+                model.IMC = model.Peso / (model.Altura * model.Altura);
+                model.Classificacao = ClassificarIMC(model.IMC);
+            }
+            return View("Index", model);
+        }
+
+        private string ClassificarIMC(double imc)
+        {
+            if (imc < 18.5)
+                return "Abaixo do peso";
+            else if (imc >= 18.5 && imc < 24.9)
+                return "Peso normal";
+            else if (imc >= 25 && imc < 29.9)
+                return "Sobrepeso";
+            else if (imc >= 30 && imc < 34.9)
+                return "Obesidade Grau I";
+            else if (imc >= 35 && imc < 39.9)
+                return "Obesidade Grau II";
+            else
+                return "Obesidade Grau III";
+        }
+    }
+}
---
+++ IMCCalculator/Models/IMCModel.cs
@@ -0,0 +1,10 @@
+﻿namespace IMCCalculator.Models
+{
+    public class IMCModel
+    {
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+        public double IMC { get; set; }
+        public string Classificacao { get; set; }
+    }
+}
